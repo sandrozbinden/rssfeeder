6d5bf02d2af34dbebb7a111761d49e1871b47bb0
Sandro Zbinden <sandrozbinden@gmail.com>
2015-05-29 20:55:36 +0200

    In case a line can't be processed write the line that can't be processed to the indexErrorDirectory
    After a file has been processed move it to the indexProcessedDirectory

--- src/main/java/com/sandrozbinden/solr/SolrIndexer.java
+++ src/main/java/com/sandrozbinden/solr/SolrIndexer.java
@@ -2,14 +2,51 @@ package com.sandrozbinden.solr;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.common.SolrInputDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
 import com.sandrozbinden.rss.Feed;
+import com.sandrozbinden.rss.FeedParser;
+import com.sandrozbinden.rss.exception.InvalidFeedInput;
 
 public class SolrIndexer {
+
+    private static final Logger logger = LoggerFactory.getLogger(SolrIndexer.class);
+
+    public void indexFeedFiles(File indexInputDirectory, File indexProcessedDirectory, File indexErrorDirectory) throws IOException, SolrServerException {
+        File rootDirectory = new File("src/test/resources/data");
+        logger.info("Importing files from directory: " + indexInputDirectory.getCanonicalPath());
+        FeedParser feedParser = new FeedParser();
+        File[] textFiles = rootDirectory.listFiles();
+        int fileCounter = 0;
+        for (File textFile : textFiles) {
+            logger.info("Processing file: " + textFile.getName() + " file " + fileCounter + " of " + textFiles.length);
+            List<String> lines = Files.readLines(textFile, Charsets.UTF_8);
+            int lineCounter = 0;
+            for (String line : lines) {
+                try {
+                    index(feedParser.parse(line));
+                } catch (InvalidFeedInput e) {
+                    logger.warn("Can't add feed: " + lineCounter + " of file " + textFile.getName());
+                    Files.append(line, new File(indexErrorDirectory, textFile.getName()), Charsets.UTF_8);
+                }
+                lineCounter = lineCounter + 1;
+                if (lineCounter % 100 == 0) {
+                    logger.info(" Processed " + lineCounter + " of " + lines.size() + " feeds");
+                }
+            }
+            logger.info("Finished processing file: " + textFile.getName() + " imported " + lines.size() + "feeds");
+            fileCounter = fileCounter + 1;
+            Files.move(textFile, new File(indexProcessedDirectory, textFile.getName()));
+        }
+    }
 
     public void index(Feed feed) throws SolrServerException, IOException {
         SolrClient client = SolrClientLocator.getSolrClient();
@@ -25,7 +62,4 @@ public class SolrIndexer {
         client.commit();
     }
 
-    public void indexFeedFiles(File indexInputDirectory, File indexProcessedDirectory, File indexErrorDirectory) {
-        //
-    }
 }
